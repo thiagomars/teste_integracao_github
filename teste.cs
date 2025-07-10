cb284c186b56fed950cfad3745968f8a5f373afe
Thiago Marques Sousa <50245545+thiagomars@users.noreply.github.com>
2025-07-10 15:52:12 -0300

    To define any custom component within an MDX file, you have to export it: only paragraphs that start with export will be parsed as components instead of prose.
    Apart from importing a component and exporting a component, a third way to use a component in MDX is to register it to the global scope, which will make it automatically available in every MDX file, without any import statements.

--- teste.cs
+++ teste.cs
@@ -6,7 +6,7 @@ using BOX3_ERP_API.Core.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System;
 // using System.Collections.Generic;
-using System.Linq;
+// using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Dropbox.Api.Files.ListRevisionsMode;
